ac6a1655d732fe18ec4b24347ec73703290288b1
Mcvriez <mcvriez@gmail.com>
2019-08-20 09:11:19 +0300

    ex 17 page 130 mode of strings

--- playground.cpp
+++ playground.cpp
@@ -1,23 +1,17 @@
-// Find mode (the number that appears most) in the array
+// Find mode (the number that appears most) in the array of strings
 #include "std_lib_facilities.h"
 #include <stdlib.h>
 #include <time.h>
 
-vector <int> numbers = {};
+vector <string> strings = { "4BBXP6G381D3", "H05GEGTVZQ3M", "194RXOIBWZJN", "123", "LY4E8ITRM04S", "VHTI7KUBBVWH", "8RGX2VNC4UIY", "HZ4OPHZ3S5HG", "9Z39Q348AKK3", "JEASAMLBFLGJ", "3N5OOJB1369T", "123", "GHNXR6PLV9IZ", "5MFCE11U0G05", "123", "123", "abc", "abs", "asdfasdf", "asdfa", "sdf", "sadf", "asd", "fa", "sdf", "asd", "f", "asdf", "as", "df", "asd", "fw43", "wer", "s ", "werf", "sd", "f", "asdf", "asd", "f", "sadf", "sad", "fsa", "df", "sad", "f", "ag", "fg", "hj", "fgdh", "dfgs", "h", "sadfgs", "dfg", "g", "afdgdfg", "dfg", "sdf", "gh", "sgfhj", "ghd", "j", "gdhj", "gh", "j", "hk", "hn", "m", "vbm", "vbnm", "bvn", "m", "bvnm", "v", "nbm", "bvnm", "ytr", "rty", "sdfsh", };
 
-void initialize(int max, int step){
-	for (int index = 0; index < max; ++index) {
-		int randint = rand() % step + 1;
-		numbers.push_back(randint);
-	}
-}
 
-int find_mode() {
+string find_mode() {
 	int max = 0;
 	int current = 0;
-	int mode = 0;
-	for (int index = 1; index < numbers.size(); ++index) {
-		if (numbers[index] == numbers[index - 1]) {
+	string mode = "";
+	for (int index = 1; index < strings.size(); ++index) {
+		if (strings[index] == strings[index - 1]) {
 			++current;
 		}
 		else {
@@ -25,22 +19,18 @@ int find_mode() {
 		}
 		if (current > max) {
 			max = current;
-			mode = numbers[index];
+			mode = strings[index];
 		}
 	}
 	return mode;
 }
 
 int main() {
-    int max_numbers = 200;
-    int number_cap = 100;
-	srand((int)time(0));
-	initialize(max_numbers, number_cap);
-	sort(numbers);
+	sort(strings);
 	cout << "test initialize:\n";
-	for (int x : numbers) {
+	for (string x : strings) {
 		cout << x << ", ";
 	}
-	int mode = find_mode();
-	cout << "\n\nMode number is: " << mode << ".\n";
+	string mode = find_mode();
+	cout << "\n\nMode string is: " << mode << " min is: " <<strings[0] << " max is: " << strings[strings.size() -1] <<".\n";
 }       
