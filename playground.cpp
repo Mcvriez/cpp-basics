5d9baa8160029c82c07889176b05ae240b4914c6
Mcvriez <mcvriez@gmail.com>
2019-08-20 10:00:45 +0300

    ex 18 p 130 quadratic equation

--- playground.cpp
+++ playground.cpp
@@ -1,36 +1,25 @@
-// Find mode (the number that appears most) in the array of strings
+// Quadratic equations solver
 #include "std_lib_facilities.h"
-#include <stdlib.h>
-#include <time.h>
-
-vector <string> strings = { "4BBXP6G381D3", "H05GEGTVZQ3M", "194RXOIBWZJN", "123", "LY4E8ITRM04S", "VHTI7KUBBVWH", "8RGX2VNC4UIY", "HZ4OPHZ3S5HG", "9Z39Q348AKK3", "JEASAMLBFLGJ", "3N5OOJB1369T", "123", "GHNXR6PLV9IZ", "5MFCE11U0G05", "123", "123", "abc", "abs", "asdfasdf", "asdfa", "sdf", "sadf", "asd", "fa", "sdf", "asd", "f", "asdf", "as", "df", "asd", "fw43", "wer", "s ", "werf", "sd", "f", "asdf", "asd", "f", "sadf", "sad", "fsa", "df", "sad", "f", "ag", "fg", "hj", "fgdh", "dfgs", "h", "sadfgs", "dfg", "g", "afdgdfg", "dfg", "sdf", "gh", "sgfhj", "ghd", "j", "gdhj", "gh", "j", "hk", "hn", "m", "vbm", "vbnm", "bvn", "m", "bvnm", "v", "nbm", "bvnm", "ytr", "rty", "sdfsh", };
-
-
-string find_mode() {
-	int max = 0;
-	int current = 0;
-	string mode = "";
-	for (int index = 1; index < strings.size(); ++index) {
-		if (strings[index] == strings[index - 1]) {
-			++current;
-		}
-		else {
-			current = 0;
-		}
-		if (current > max) {
-			max = current;
-			mode = strings[index];
-		}
-	}
-	return mode;
-}
 
 int main() {
-	sort(strings);
-	cout << "test initialize:\n";
-	for (string x : strings) {
-		cout << x << ", ";
+	cout << "Quadratic equation is equation with the form ax2 + bx + c = 0, and a is not zero.\n";
+	cout << "Please write a, b and c separated with whitespace.\n";
+	double a = 1;
+	double b = 0;
+	double c = 0;
+	cin >> a >> b >> c;
+	double discriminant = b * b - a * c;
+	
+	if (a == 0) {
+		cout << "a can't be equal to zero!\n";
 	}
-	string mode = find_mode();
-	cout << "\n\nMode string is: " << mode << " min is: " <<strings[0] << " max is: " << strings[strings.size() -1] <<".\n";
+	else if (discriminant > 0) {
+		double x1 = (b * -1 + sqrt(discriminant)) / 2 * a;
+		double x2 = (b * -1 - sqrt(discriminant)) / 2 * a;
+		cout << "Equation has two roots: " << x1 << " and " << x2 << "\n";
+	}
+	else if (discriminant == 0) {
+		int x = (b * -1) / 2 * a;
+		cout << "Equation has one root: " << x << "\n";
+	}
 }       
