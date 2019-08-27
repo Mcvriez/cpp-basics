e210fb0c605adf2754276f7f49473f80efbc2cc9
Mcvriez <mcvriez@gmail.com>
2019-08-27 08:47:30 +0300

    Chapter 5 excercise 11 Fibonacci + input check

--- chapter_5_Errors.cpp
+++ chapter_5_Errors.cpp
@@ -1,44 +1,21 @@
-// Chapter 5 excercise 10 sum of doubles
+// Chapter 5 excercise 11 Fibonacci
 #include "std_lib_facilities.h"
 const int max_int = 2147483647;
 
 int main() {
-	cout << "Sum of first N integers\n";
-	int number = 0;
-	double read = 0;
-	double sum = 0;
-	vector <double> values = {};
-	vector <double> diff = {};
-
-	cout << "Please enter number (positive and integer) of values you want to sum:\n";
-	cin >> number;
-	if (number < 1 && cin) {
-		cout << "Number should be positive.\n";
-	}
-	else {
-		cout << "Please enter some integers (press any character to stop):\n";
-		while (cin >> read) {
-			if (cin) {
-				cout << read << " was added.\n";
-				values.push_back(read);
-			}
-			else {
-				cout << "Read error.\n";
-			}
-		}
-		if (values.size() < number) {
-			cout << "Number of values should be less than array of values.\n";
-		}
-		else {
-			for (int index = 0; index < number; ++index) {
-				sum += values[index];
-			}
-			cout << "The sum of first " << number << " elements is: " << sum << "\nDiff is:\n";
-			for (int index = 1; index < values.size(); ++index) {
-				diff.push_back(values[index] - values[index - 1]);
-				cout << values[index] - values[index - 1] << " ";
-			}
-			cout << "\n";
-		}
+	int max = 0;
+	cout << "Enter max Fibonacci numbers you want to see:\n";
+	cin >> max;
+	if (!cin) error("Wrong input\n");
+	int first = 1;
+	int second = 1;
+	int sum = 0;
+	cout << first << "\t";
+	while (max_int - second > first && max > 0) {
+		cout << second << "\t";
+		sum = first + second;
+		first = second;
+		second = sum;
+		--max;
 	}
 }
