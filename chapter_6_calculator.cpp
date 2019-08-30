20ca172519b7d5b265e26285e2a17eb02f635692
Mcvriez <mcvriez@gmail.com>
2019-08-30 10:22:03 +0300

    chapter 6 drill p1 page 217 - first working version

--- chapter_6_calculator.cpp
+++ chapter_6_calculator.cpp
@@ -1,90 +1,177 @@
-// Chapter 6 excercise 10
 #include "std_lib_facilities.h"
 
-/*A permutation is an ordered subset of a set. For example, say you wanted
-to pick a combination to a vault. There are 60 possible numbers, and
-you need three different numbers for the combination. There are P(60,3)
-permutations for the combination, where P is defined by the formula:
 
-P (a, b) = a! / (a − b)!
-
-where ! is used as a suffix factorial operator. For example, 4! is 4*3*2*1.
-Combinations are similar to permutations, except that the order of the
-objects doesn’t matter. For example, if you were making a “banana split”
-sundae and wished to use three different flavors of ice cream out of five
-that you had, you wouldn’t care if you used a scoop of vanilla at the
-beginning or the end; you would still have used vanilla. The formula for
-combinations is:
-
-C (a, b) = P (a,b) / b!
+class Token {
+public:
+	char kind;        // what kind of token
+	double value;     // for numbers: a value 
+	Token(char ch)    // make a Token from a char
+		:kind(ch), value(0) { }
+	Token(char ch, double val)     // make a Token from a char and a double
+		:kind(ch), value(val) { }
+};
 
 
-Design a program that asks users for two numbers, asks them whether
-they want to calculate permutations or combinations, and prints out the
-result. This will have several parts. Do an analysis of the above require-
-ments. Write exactly what the program will have to do. Then, go into
-the design phase. Write pseudo code for the program, and break it into
-sub-components. This program should have error checking. Make sure
-that all erroneous inputs will generate good error messages.*/
+class Token_stream {
+public:
+	Token_stream();   // make a Token_stream that reads from cin
+	Token get();      // get a Token (get() is defined elsewhere)
+	void putback(Token t);    // put a Token back
+private:
+	bool full;        // is there a Token in the buffer?
+	Token buffer;     // here is where we keep a Token put back using putback()
+};
 
-// ask for two numbers
-// read them to two integers
-// ask what to calculate P or C
 
-int factorial(int x){
-	for (int i = x - 1; i > 0; --i){
-		x *= i;
-	}
-	return x;
+// The constructor just sets full to indicate that the buffer is empty:
+Token_stream::Token_stream()
+	:full(false), buffer(0)    // no Token in buffer
+{
 }
 
-int permutation(int x, int y) {return factorial(x) / factorial(x-y);} // P(a, b) = a!/(a−b)!
-int combination(int x, int y) {return permutation(x, y) / factorial(y);} // C(a, b) = P(a,b)/b!
+
+// The putback() member function puts its argument back into the Token_stream's buffer:
+void Token_stream::putback(Token t)
+{
+	if (full) error("putback() into a full buffer");
+	buffer = t;       // copy t to buffer
+	full = true;      // buffer is now full
+}
 
 
-void user_input(){
-	cout << "Enter two positive integers separated by white space. First number should be bigger than second:\nFirst number: ";
-	int x;
-	int y;
-	string input;
-	cin >> x;
-	cout << "Second number: ";
-	cin >> y;
-	if (!cin) error ("Invalid input - integers\n");
-	if (x < 0 || y < 0) cout << "Numbers should be positive\n";
-	else if (x < y) {
-		cout << "Number of the permutations is zero.\n\n";
+
+Token Token_stream::get()
+{
+	if (full) {       // do we already have a Token ready?
+		// remove token from buffer
+		full = false;
+		return buffer;
 	}
-	else {
-		cout << "Enter what you want to calculate - number of permutations (P) or number of combinations (C):\n";
-		cin >> input;
-		if (!cin) error ("Invalid input - result\n");
-		if (input == "C" || input == "c") {
-			int result = combination(x, y);
-			cout << "Number of combinations is: " << result << "\n\n";
-		}
-		else if (input == "P" || input == "p") {
-			int result = permutation(x,y);
-			cout << "Number of permutations is: " << result << "\n\n";
-		}
-		else cout << "Invalid input - type of operation. Try again\n";
+
+	char ch;
+	cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
+
+	switch (ch) {
+	case ';':    // for "print"
+	case 'q':    // for "quit"
+	case '(': case ')': case '+': case '-': case '*': case '/':
+		return Token(ch);        // let each character represent itself
+	case '.':
+	case '0': case '1': case '2': case '3': case '4':
+	case '5': case '6': case '7': case '8': case '9':
+	{
+		cin.putback(ch);         // put digit back into the input stream
+		double val;
+		cin >> val;              // read a floating-point number
+		return Token('8', val);   // let '8' represent "a number"
+	}
+	default:
+		error("Bad token");
 	}
 }
 
-int main() {
-	while (true){
-		try {
-			user_input();
-			}
-		catch (exception& e) {
-		cerr << "Error: " << e.what() << endl;
-		keep_window_open();
-		return 1;
+
+Token_stream ts;        // provides get() and putback() 
+
+double expression();    // declaration so that primary() can call expression()
+
+// deal with numbers and parentheses
+double primary()
+{
+	Token t = ts.get();
+	switch (t.kind) {
+	case '(':    // handle '(' expression ')'
+	{
+		double d = expression();
+		t = ts.get();
+		if (t.kind != ')') error("')' expected");
+		return d;
+	}
+	case '8':            // we use '8' to represent a number
+		return t.value;  // return the number's value
+	default:
+		error("primary expected");
+	}
+}
+
+
+// deal with *, /
+double term()
+{
+	double left = primary();
+	Token t = ts.get();        // get the next token from token stream
+
+	while (true) {
+		switch (t.kind) {
+		case '*':
+			left *= primary();
+			t = ts.get();
+			break;
+		case '/':
+		{
+			double d = primary();
+			if (d == 0) error("divide by zero");
+			left /= d;
+			t = ts.get();
+			break;
 		}
-	catch (...) {
-		cerr << "Unknown exception!\n";
-		keep_window_open();
-		return 2;
+		default:
+			ts.putback(t);     // put t back into the token stream
+			return left;
 		}
 	}
 }
+
+
+
+// deal with + and -
+double expression()
+{
+	double left = term();      // read and evaluate a Term
+	Token t = ts.get();        // get the next token from token stream
+
+	while (true) {
+		switch (t.kind) {
+		case '+':
+			left += term();    // evaluate Term and add
+			t = ts.get();
+			break;
+		case '-':
+			left -= term();    // evaluate Term and subtract
+			t = ts.get();
+			break;
+		default:
+			ts.putback(t);     // put t back into the token stream
+			return left;       // finally: no more + or -: return the answer
+		}
+	}
+}
+
+
+
+int main()
+try
+{
+	double val = 0;
+	while (cin) {
+		Token t = ts.get();
+		if (t.kind == 'q') break; // 'q' for quit
+		if (t.kind == ';')        // ';' for "print now"
+			cout << "=" << val << '\n';
+		else
+			ts.putback(t);
+		val = expression();
+	}
+	keep_window_open();
+}
+catch (exception& e) {
+	cerr << "error: " << e.what() << '\n';
+	keep_window_open();
+	return 1;
+}
+catch (...) {
+	cerr << "Oops: unknown exception!\n";
+	keep_window_open();
+	return 2;
+}
+
