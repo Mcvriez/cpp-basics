1a09e2a2a9f19614d0618e80cca55522e0b3c2ee
Mcvriez <mcvriez@gmail.com>
2019-09-06 08:24:35 +0300

    chapter 6 excersize 6 English grammar

--- chapter_6_calculator.cpp
+++ chapter_6_calculator.cpp
@@ -1,71 +1,73 @@
 #include "std_lib_facilities.h"
 /*
-Write a program where you first enter a set of name-and-value pairs, such
-as Joe 17 and Barbara 22 . For each pair, add the name to a vector called
-names and the number to a vector called scores (in corresponding po-
-sitions, so that if names[7]=="Joe" then scores[7]==17 ). Terminate input
-with NoName 0 . Check that each name is unique and terminate with an
-error message if a name is entered twice. Write out all the (name,score)
-pairs, one per line.
-Define a class Name_value that holds a string and a value. Rework exer-
-cise 19 in Chapter 4 to use a vector<Name_value> instead of two vectors.
+Write a program that checks if a sentence is correct according to the “En-
+glish” grammar in §6.4.1. Assume that every sentence is terminated by
+a full stop (.) surrounded by whitespace. For example, birds fly but the
+fish swim . is a sentence, but birds fly but the fish swim (terminating dot
+missing) and birds fly but the fish swim. (no space before dot) are not. For
+each sentence entered, the program should simply respond “OK” or “not
+OK.” Hint: Don’t bother with tokens; just read into a string using >>
 */
+vector <string> verbs = { "rules", "fly", "swim" };
+vector <string> articles = { "the" };
+vector <string> nouns = { "birds", "fish", "C++" };
+vector <string> conjuctions = { "and", "or", "but" };
 
-
-class Name_value {
-	public:
-		string name;        
-		double value;     
-		Name_value()     
-			:name(), value(0) { } 
-		Name_value(string name, double val)     
-			:name(name), value(val) { }
-		bool is_unique(string testname);
-};
-
-vector <Name_value> names;
-
-bool Name_value::is_unique(string testname){
-			for (Name_value nv: names) {
-				if (nv.name == testname) return false;
-			}
-		return true;
-		}
-
-void terminate(){
-	cout << "Vector contains  following names:\n";
-	for (Name_value nv: names){
-		cout << nv.name << " " << nv.value << "\n";
+bool is_verb(string word){
+	for (string x : verbs) {
+		if (word == x) return true;
 	}
+	return false;
+}
+
+bool is_article(string word) {
+	for (string x : articles) {
+		if (word == x) return true;
+	}
+	return false;
+}
+
+bool is_noun(string word) {
+	for (string x : nouns) {
+		if (word == x) return true;
+	}
+	return false;
+}
+
+bool is_conjuction(string word) {
+	for (string x : conjuctions) {
+		if (word == x) return true;
+	}
+	return false;
+}
+
+int sentence() {
+	string w1; string w2; string w3; string w4;
+	cin >> w1;
+	cout << w1 << " : first word\n";
+	if (is_article(w1)) { cin >> w2; }
+	else if (is_noun(w1)) { w2 = w1; }
+	cout << w2 << " : second word\n";
+	if (is_noun(w2)) {
+		cin >> w3;
+		cout << w3<< " : third word\n";
+		if (is_verb(w3)) {
+			cin >> w4;
+			cout << w4 << " : forth word\n";
+			if (w4 == ".") { return 0; }
+			if (is_conjuction(w4)) { cout << "conjuction found: " << w4 << "\n"; return sentence(); }			
+		}
+	}
+	while (w1 != ".") {
+		cin >> w1;
+	}
+	return 1;
 }
 
 int main() {
-	try
-	{	
-		cout << "Enter pairs of names and values. Each name should be unique. Enter NoName 0 to exit.\n";
-		while (true)
-		{
-			Name_value nv;
-			cin >> nv.name >> nv.value;
-			if (nv.name == "NoName" && nv.value == 0){
-				terminate();
-				return 0;
-			}
-			if (nv.is_unique(nv.name)) {
-				names.push_back(nv);
-			}
-			else {error("Name is not unique");}
-		}
-	}
-	catch (exception& e) {
-		cerr << "error: " << e.what() << '\n';
-		keep_window_open();
-		return 1;
-	}
-	catch (...) {
-		cerr << "Oops: unknown exception!\n";
-		keep_window_open();
-		return 2;
+	string word;
+	while (true) {
+		if (sentence() == 0) cout << "OK\n";
+		else cout << "Not OK\n";
 	}
 }
-
