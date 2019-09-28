443ad2d7da111caa4ec5bfaf78c5abe8e0933fed
Mcvriez <mcvriez@gmail.com>
2019-09-28 00:45:11 -0700

    ex 2 ch 9 - Name_pairs class with read_names, read_ages, print, sort

--- chapter_9_technicalities.cpp
+++ chapter_9_technicalities.cpp
@@ -19,27 +19,102 @@ vector to match.
 Implement all “operations” as member functions. Test the class (of course: test early and often).
 
 */
-
+// Year(int x) : y{ x } { if (x < min || max <= x) throw Invalid{}; }
 
 class Name_pairs
 {
 	public:
-		Name_pairs();
+		Name_pairs(istream& input, ostream& output, int ms) : is{ input }, os{ output }, max_size { ms } {};
+		void read_names();
+		void read_ages();
+		void print();
+		void sort();
 	private:
-		string name;
-		double age;
+		istream& is; 
+		ostream& os;
+		int max_size;
+		vector <string> names;
+		vector <double> ages;
 };
 
-Name_pairs::Name_pairs()
-{
+void Name_pairs::read_names() {
+	string name;
+	os << "Enter names separated by whitespace or newlines. Maximum " << max_size << " names.\nPress X to stop prematurely." << endl;
+	while (names.size() < max_size && is >> name && name != "X") {
+		names.push_back(name);
+	}
+};
+
+void Name_pairs::read_ages() {
+	double age;
+	int s = names.size();
+	if (s < 1) {
+		os << "Names vector is empty." << endl; 
+		return;
+	}
+	os << "Enter ages for the each name." << endl;
+	for (int i = 0; i < s; ++i) {
+		os << names[i] << ": "; 
+		if (is >> age) {
+			ages.push_back(age);
+			if (ages.size() == s) return;
+		}
+		else  error("Invalid input - age");
+	}
+};
+
+void Name_pairs::print() {
+	os << endl << "names and ages:" << endl;
+	if (!names.size() || !ages.size() || ages.size() != names.size()) {
+		os << "Namepairs object is empty or corrupted." << endl;
+		return;
+	}
+		for (int i = 0; i < names.size(); ++i) {
+		os << names[i] << ": " << ages[i] << '\t';
+	}
+	os << endl;
 }
 
 
+void Name_pairs::sort() {
+	os << "\nsorting names.." << endl;
+	if (!names.size() || !ages.size() || ages.size() != names.size()) {
+		os << "Namepairs object is empty or corrupted." << endl;
+		return;
+	}
+	vector <string> copys;
+	vector <double> copyd;
+
+	for (string name : names) copys.push_back(name);
+	for (double age : ages) copyd.push_back(age);
+	
+	std::sort(names.begin(), names.end());
+	
+	for (int i = 0; i < copys.size(); ++i) {
+		for (int j = 0; j < copys.size(); ++j) {
+			if (names[i] == copys[j]) ages[i] = copyd[j];
+		}
+	}
+}
+
+
+int routine (Name_pairs np) {
+	np.read_names();
+	np.read_ages();
+	np.print();
+	np.sort();
+	np.print();
+	return 0;
+}
+
+const int big_size = 2;
 
 int main() {
 	try {
-
-		return 0;
+		Name_pairs np(cin, cout, big_size);
+		routine(np);
+		Name_pairs np2(cin, cout, big_size);
+		routine(np2);
 	}
 	catch (exception& e) {
 		cerr << "Error: " << e.what() << endl;
