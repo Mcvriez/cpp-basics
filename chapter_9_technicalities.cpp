f69d1528665a8f3cdc0f75d00201451671823fb5
Mcvriez <mcvriez@gmail.com>
2019-09-30 00:16:16 -0700

    ex 7 ch 9 adding enum class Genre

--- chapter_9_technicalities.cpp
+++ chapter_9_technicalities.cpp
@@ -2,21 +2,26 @@
 
 /*
 
-Add operators for the Book class. 
-Have the == operator check whether the ISBN numbers are the same for two books.
-Have != also compare the ISBN numbers. 
-Have a << print out the title, author, and ISBN on separate lines.
+Create an enumerated type for the Book class called Genre. Have the types be fiction, nonfiction, periodical,
+biography, and children. Give each book a Genre and make appropriate changes to the Book constructor and member
+functions.
 
 */
 
 namespace Bookstore {
+
+	enum class Genre {
+		fiction = 0, nonfiction, periodical, biography, children
+	};
 	
 	struct ISBN {
-		 string id;
+	private:
+		string id;
 		const int n0;
 		const int n1;
 		const int n2;
 		const string s;
+	public:
 		ISBN(int nn0, int nn1, int nn2, string ss) : 
 			n0{ nn0 }, 
 			n1{ nn1 }, 
@@ -27,17 +32,19 @@ namespace Bookstore {
 			// cout << "creating ISBN" << endl;
 			id = to_string(n0) + "-" + to_string(n1) + "-" + to_string(n2) + s;
 		};
+		const string get_id()const { return id; }
 	};
 
 	class Book
 	{
 	public:
 		Book();
-		Book(string author, string copyright, ISBN isbn, string title);
+		Book(string author, string copyright, ISBN isbn, string title, Genre ge);
 		const string get_author() const { return author; };
 		const string get_copyright() const { return copyright; };
 		const string get_isbn() const { return isbn; };
 		const string get_title() const { return title; };
+		const Genre get_genre() const { return genre; };
 		const bool is_checked() const { return checked; };
 		void check() { checked = true; return; };
 		void uncheck() { checked = false; return; };
@@ -46,6 +53,7 @@ namespace Bookstore {
 		const string copyright;
 		const string isbn;
 		const string title;
+		const Genre genre;
 		bool checked;
 	};
 
@@ -54,14 +62,16 @@ namespace Bookstore {
 		copyright{ "Creative Commons Corporation" },
 		isbn{ "000-0-000-xxx" },
 		title{ "0 book" },
+		genre {Genre::fiction},
 		checked{ false }
 	{};
 
-	Book::Book(string author, string copyright, ISBN isbn, string title) :
+	Book::Book(string author, string copyright, ISBN isbn, string title, Genre gen) :
 		author{ author },
 		copyright{ copyright },
-		isbn{ isbn.id },
+		isbn{ isbn.get_id() },
 		title{ title },
+		genre{ gen },
 		checked{ false }
 	{
 		//cout << "creating book" << endl; 
@@ -83,6 +93,21 @@ ostream& operator << (ostream& os, Bookstore::Book& b) {
 	cout << "Copyright: " << b.get_copyright() << endl;
 	cout << "ISBN: " << b.get_isbn() << endl;
 	cout << "Book is checked: " << b.is_checked() << endl;
+	Bookstore::Genre gen = b.get_genre();
+	switch (gen){
+		// nonfiction, periodical, biography, children
+		case Bookstore::Genre::fiction:
+			cout << "Genre: fiction" << endl; break;
+		case Bookstore::Genre::nonfiction:
+			cout << "Genre: nonfiction" << endl;  break;
+		case Bookstore::Genre::periodical:  break;
+			cout << "Genre: periodical" << endl;  break;
+		case Bookstore::Genre::biography:
+			cout << "Genre: biography" << endl;  break;
+		case Bookstore::Genre::children:
+			cout << "Genre: children" << endl;  break;
+	}
+	cout << "--------------" << endl;
 	return os;
 }
 
@@ -91,12 +116,14 @@ ostream& operator << (ostream& os, Bookstore::Book& b) {
 int main() {
 	try {
 		Bookstore::ISBN book_id = Bookstore::ISBN(123, 4, 321, "kek");
-		Bookstore::Book book = Bookstore::Book("Stephen King", "No License 2019", book_id, "The Stand");
+		Bookstore::Book book = Bookstore::Book("Stephen King", "No License 2019", book_id, "The Stand", Bookstore::Genre::fiction);
+
 		Bookstore::ISBN book_id1 = Bookstore::ISBN(1223, 4, 321, "kek");
-		Bookstore::Book book1 = Bookstore::Book("Stephen King", "No License 2019", book_id1, "The Stand");
+		Bookstore::Book book1 = Bookstore::Book("Stephen King", "No License 2019", book_id1, "It", Bookstore::Genre::children);
+		book1.check();
+
 		cout << book;
-		if (book1 == book) { cout << "Equal " << endl; }
-		if (book1 != book) { cout << "Not equal " << endl; }
+		cout << book1;
 	}
 	catch (exception& e) {
 		cerr << "Error: " << e.what() << endl;
