656fda50d5eb447db33d24aa2d2ec49db12f8db3
Mcvriez <mcvriez@gmail.com>
2019-09-30 22:30:28 -0700

    ex 8 ch 9 class Patron

--- chapter_9_technicalities.cpp
+++ chapter_9_technicalities.cpp
@@ -2,18 +2,24 @@
 
 /*
 
-Create an enumerated type for the Book class called Genre. Have the types be fiction, nonfiction, periodical,
-biography, and children. Give each book a Genre and make appropriate changes to the Book constructor and member
-functions.
+Create a Patron class for the library. 
+The class will have a:
+- user’s name 
+- library card number
+- and library fees (if owed)
+
+Have functions that access this data, as well as a function to set the fee of the user. 
+Have a helper function that returns a Boolean (bool) depending on whether or not the user owes a fee
 
 */
 
-namespace Bookstore {
+namespace Library {
 
 	enum class Genre {
 		fiction = 0, nonfiction, periodical, biography, children
 	};
-	
+
+
 	struct ISBN {
 	private:
 		string id;
@@ -22,9 +28,9 @@ namespace Bookstore {
 		const int n2;
 		const string s;
 	public:
-		ISBN(int nn0, int nn1, int nn2, string ss) : 
-			n0{ nn0 }, 
-			n1{ nn1 }, 
+		ISBN(int nn0, int nn1, int nn2, string ss) :
+			n0{ nn0 },
+			n1{ nn1 },
 			n2{ nn2 },
 			s{ ss },
 			id{ "" }
@@ -34,6 +40,26 @@ namespace Bookstore {
 		};
 		const string get_id()const { return id; }
 	};
+
+
+	class Patron
+	{
+	public:
+		Patron(string name, int numb) : username{ name }, number{ numb }, fee{0} {};
+		const string get_username() const { return username; };
+		const int get_number() const { return number; };
+		const double get_fee() const { return fee; };
+		void set_fee(double new_fee) { 
+			if (new_fee >= 0) fee = new_fee;
+			else cout << "Setting fee had failed. You can't have negative fee: " << new_fee << endl;
+		}
+	private:
+		const string username;
+		const int number;
+		double fee;
+	};
+
+
 
 	class Book
 	{
@@ -78,33 +104,37 @@ namespace Bookstore {
 	};
 
 
+	bool has_debt(Patron& user) {
+		double fee = user.get_fee();
+		if (fee) return true;
+		return false;
+	}
 }
 
-bool operator == (const Bookstore::Book& b1, const Bookstore::Book& b2) {
+bool operator == (const Library::Book& b1, const Library::Book& b2) {
 	return b1.get_isbn() == b2.get_isbn();
 }
-bool operator != (const Bookstore::Book& b1, const Bookstore::Book& b2) {
+bool operator != (const Library::Book& b1, const Library::Book& b2) {
 	return b1.get_isbn() != b2.get_isbn();
 }
 
-ostream& operator << (ostream& os, Bookstore::Book& b) {
+ostream& operator << (ostream& os, Library::Book& b) {
 	cout << "Author: " << b.get_author() << endl;
 	cout << "Title: " << b.get_title() << endl;
 	cout << "Copyright: " << b.get_copyright() << endl;
 	cout << "ISBN: " << b.get_isbn() << endl;
 	cout << "Book is checked: " << b.is_checked() << endl;
-	Bookstore::Genre gen = b.get_genre();
+	Library::Genre gen = b.get_genre();
 	switch (gen){
-		// nonfiction, periodical, biography, children
-		case Bookstore::Genre::fiction:
+		case Library::Genre::fiction:
 			cout << "Genre: fiction" << endl; break;
-		case Bookstore::Genre::nonfiction:
+		case Library::Genre::nonfiction:
 			cout << "Genre: nonfiction" << endl;  break;
-		case Bookstore::Genre::periodical:  break;
+		case Library::Genre::periodical:  
 			cout << "Genre: periodical" << endl;  break;
-		case Bookstore::Genre::biography:
+		case Library::Genre::biography:
 			cout << "Genre: biography" << endl;  break;
-		case Bookstore::Genre::children:
+		case Library::Genre::children:
 			cout << "Genre: children" << endl;  break;
 	}
 	cout << "--------------" << endl;
@@ -115,12 +145,20 @@ ostream& operator << (ostream& os, Bookstore::Book& b) {
 
 int main() {
 	try {
-		Bookstore::ISBN book_id = Bookstore::ISBN(123, 4, 321, "kek");
-		Bookstore::Book book = Bookstore::Book("Stephen King", "No License 2019", book_id, "The Stand", Bookstore::Genre::fiction);
+		using namespace Library;
+		ISBN book_id = ISBN(123, 4, 321, "kek");
+		Book book = Book("Stephen King", "No License 2019", book_id, "The Stand", Genre::fiction);
 
-		Bookstore::ISBN book_id1 = Bookstore::ISBN(1223, 4, 321, "kek");
-		Bookstore::Book book1 = Bookstore::Book("Stephen King", "No License 2019", book_id1, "It", Bookstore::Genre::children);
+		ISBN book_id1 = ISBN(1223, 4, 321, "kek");
+		Book book1 = Book("Stephen King", "No License 2019", book_id1, "It", Genre::children);
 		book1.check();
+
+		Patron user = Patron("Anna", 4312);
+		user.set_fee(-120);
+		cout << user.get_username() + " has " <<  user.get_number() << " id." << endl;
+		cout << user.get_username() + " has a debt: " << has_debt(user) <<  endl;
+		cout << "Her debt is: " << user.get_fee() << endl << endl;
+
 
 		cout << book;
 		cout << book1;
