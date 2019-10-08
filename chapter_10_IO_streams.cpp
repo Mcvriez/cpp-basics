5246090df9597152187ddbc7ea72c0e5ab96cea7
Mcvriez <mcvriez@gmail.com>
2019-10-08 00:13:33 -0700

    ex 3 ch 10 reading + median and mean

--- chapter_10_IO_streams.cpp
+++ chapter_10_IO_streams.cpp
@@ -2,14 +2,13 @@
 
 /*
 
-Write a program that creates a file of data in the form of the temperature Reading type defined in §10.5. For testing, fill
-the file with at least 50 “temperature readings.” Call this program store_temps.cpp and the file it creates
-raw_temps.txt
+Write a program that reads the data from raw_temps.txt created in exercise 2 into a vector and then calculates the
+mean and median temperatures in your data set. Call this program temp_stats.cpp
 
 */
 
-const string input = "C:\\Users\\arcady\\source\\repos\\cpp-basics\\input.txt";
-const string output = "C:\\Users\\arcady\\source\\repos\\cpp-basics\\raw_temps.txt";
+// const string input = "C:\\Users\\arcady\\source\\repos\\cpp-basics\\input.txt";
+const string input = "C:\\Users\\arcady\\source\\repos\\cpp-basics\\raw_temps.txt";
 
 struct Reading {
 	Reading() : hour{ -1 }, temperature{ -1000000000 } {};
@@ -18,11 +17,13 @@ struct Reading {
 	double temperature; // in Fahrenheit
 };
 
+// \t{'6': '4.43'},\n
 
 ifstream& operator >> (ifstream& ifs, Reading& r){
 	int i; double j;
-	ifs >> i >> j;
-	if (!ifs) {
+	char ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8;
+	ifs >> ch1 >> ch2 >> i >> ch3 >> ch4 >> ch5 >> j >> ch6 >> ch7 >> ch8;
+	if (!ifs || ch1 != '{' || ch2 != '\'' || ch3 != '\'' || ch4 != ':' || ch5 != '\'' || ch6 != '\'' || ch7 != '}' || ch8 != ',') {
 		if (ifs.eof()) return ifs;
 		else {
 			ifs.clear();
@@ -46,6 +47,33 @@ void print_v(const vector<Reading>& v) {
 	for (Reading r : v) {
 		cout << '[' << r.hour << "->" << r.temperature << "], ";
 	}
+	cout << endl;
+}
+
+double mean(const vector<Reading>& v) {
+	int sum = 0;
+	for (Reading r : v) {
+		sum += r.temperature;
+	}
+	return sum / v.size();
+}
+
+double median(const vector<Reading>& v) {
+	vector <double> temps;
+	for (Reading r : v) {
+		temps.push_back(r.temperature);
+	}
+	sort(temps);
+	if (temps.size() < 1) {
+		cout << "Empty array" << endl; 
+		return 0;
+	}
+	if (temps.size() == 1) {
+		return temps[0];
+	}
+	if (temps.size() % 2) return temps[temps.size() / 2];
+	double res = (temps[temps.size() / 2] + temps[temps.size() / 2 - 1]) / 2;
+	return res;
 }
 
 
@@ -61,12 +89,8 @@ int main() {
 			if (r.hour != -1) results.push_back(r);
 		}
 		print_v(results);
-		ofstream ost{ output }; 
-		if (!ost) error("can't open output file ", output);
-		ost << "'temperatures': {\n";
-		for (Reading r : results)
-			ost << r;
-		ost << "};";
+		cout << "temperature mean: " << mean(results) << endl;
+		cout << "temperature median: " << median(results) << endl;
 	}
 	catch (exception& e) {
 		cerr << "Error: " << e.what() << endl;
