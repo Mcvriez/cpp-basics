4b8d5845abc0dccb28ea621b56ad5c975e21eb00
Mcvriez <mcvriez@gmail.com>
2019-10-16 09:02:03 +0300

    ex 6 ch 11 - sorting punctuation

--- chapter_11_customizing_IO.cpp
+++ chapter_11_customizing_IO.cpp
@@ -1,36 +1,34 @@
 #include "std_lib_facilities.h"
 /*
 
-5. Write a program that reads strings and for each string outputs the character classification of each character, as defined by
-the character classification functions presented in §11.6. Note that a character can have several classifications (e.g., x is
-both a letter and an alphanumeric).
+6. Write a program that replaces punctuation with whitespace. Consider . (dot), ; (semicolon), , (comma), ? (question
+mark), - (dash), ' (single quote) punctuation characters. Don’t modify characters within a pair of double quotes ("). For
+example, “- don't use the as-if rule.” becomes “ don t use the as if rule ”.
 
 */
 
-string path = "C:\\Users\\arcady\\source\\repos\\cpp-basics\\source.log";
+string in = "C:\\Users\\arcady\\source\\repos\\cpp-basics\\source.log";
+string out = "C:\\Users\\arcady\\source\\repos\\cpp-basics\\result.txt";
 
 int main() {
 
 	try {
-		ifstream input { path };
-		if (!input) cout << "Can't open file " << path << endl;
-		string word;
-		while (getline(input, word)) {
-			for (char ch : word) {
-				cout << "Character " << ch << " classification:" << endl;
-				if (isspace(ch)) cout << "isspace" << '\t';
-				if (isalpha(ch)) cout << "isalpha" << '\t';
-				if (isdigit(ch)) cout << "isdigit" << '\t';
-				if (isxdigit(ch)) cout << "isxdigit" << '\t';
-				if (isupper(ch)) cout << "isupper" << '\t';
-				if (islower(ch)) cout << "islower" << '\t';
-				if (isalnum(ch)) cout << "isalnum" << '\t';
-				if (iscntrl(ch)) cout << "iscntrl" << '\t';
-				if (ispunct(ch)) cout << "ispunct" << '\t';
-				if (isprint(ch)) cout << "isprint" << '\t';
-				if (isgraph(ch)) cout << "isgraph" << '\t';
-				cout << endl;
+		ifstream input { in };
+		if (!input) cout << "Can't open file " << in << endl;
+		ofstream output{ out };
+		if (!output) cout << "Can't open file " << out << endl;
+
+		char ch;
+		string result;
+		while (input.get(ch)) {
+			if (ch == '"') {
+				output << ch;
+				while (input.get(ch) && ch != '"') // skipping
+					output << ch;
+				output << ch;
 			}
+			if (!ispunct(ch)) { output << ch; }
+			else output << ' ';
 		}
 	}
 		
