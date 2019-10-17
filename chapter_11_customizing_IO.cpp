dbc287bad5c53ec87d8ab5a18d2196fe43d2413f
Mcvriez <mcvriez@gmail.com>
2019-10-17 09:50:51 +0300

    ex 11 ch 11 word splitter with a sugar

--- chapter_11_customizing_IO.cpp
+++ chapter_11_customizing_IO.cpp
@@ -1,30 +1,60 @@
 #include "std_lib_facilities.h"
 /*
 
-10. Write a function vector<string> split(const string& s) that returns a vector of whitespace-separated substrings
-from the argument s.
+11. Write a function vector<string> split(const string& s, const string& w) that returns a vector of whitespace-
+separated substrings from the argument s, where whitespace is defined as “ordinary whitespace” plus the characters in
+w.
 
 */
 
-const string source = "C:\\Users \\arcady\\sou  rce\\repos\\cpp-ba  sics\\sourc e.lo g";
-string outbin = "C:\\Users\\arcady\\source\\repos\\cpp-basics\\binary.txt";
-string frombin = "C:\\Users\\arcady\\source\\repos\\cpp-basics\\frombinary.txt";
+const string source = "first !second second seco!nd !thirdth !__!irdthird        still third";
+const string del = "!__!";
+const string del2 = "!";
+const string del3 = "";
 
-vector<string> split(const string& s){
+vector<string> split(const string& s, const string& w){
 	istringstream ss{ s };
 	vector<string> v;
 	string word;
-	while (ss >> word) {
-		v.push_back(word);
+	string buff;
+	char ch;
+
+	while (ss.get(ch)) {
+		if (!isspace(ch))
+			word += ch;
+		else {
+			bool check = true;
+			buff += ch;
+			for (char n : w) {
+				ss.get(ch);
+				buff += ch;
+				if (ch != n) {
+					word += buff;
+					check = false;
+					break;
+				}
+			}
+			buff = "";
+			if (check && word != "") {
+				v.push_back(word); 
+				word = "";
+			}
+		}	
+	}
+	if (word.size()) v.push_back(word);
+	for (string word : v) {
 		cout << word << endl;
 	}
+	cout << v.size() << endl;;
 	return v;
 }
 
 int main() {
 
 	try {
-		split(source);
+		split(source, del);
+		split(source, del2);
+		split(source, del3);
 	}
 		
 	catch (exception& e) {
