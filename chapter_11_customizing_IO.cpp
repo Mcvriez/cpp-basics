1d6f5ff5d81939d4b753bf3c1297e25d249babf6
Mcvriez <mcvriez@gmail.com>
2019-10-17 09:12:06 +0300

    ex 10 ch 11 word splitter with istringstream usage

--- chapter_11_customizing_IO.cpp
+++ chapter_11_customizing_IO.cpp
@@ -1,46 +1,30 @@
 #include "std_lib_facilities.h"
 /*
 
-9. Split the binary I/O program from §11.3.2 into two: one program that converts an ordinary text file into binary and one
-program that reads binary and converts it to text. Test these programs by comparing a text file with what you get by
-converting it to binary and back.
+10. Write a function vector<string> split(const string& s) that returns a vector of whitespace-separated substrings
+from the argument s.
 
 */
 
-string source = "C:\\Users\\arcady\\source\\repos\\cpp-basics\\source.log";
+const string source = "C:\\Users \\arcady\\sou  rce\\repos\\cpp-ba  sics\\sourc e.lo g";
 string outbin = "C:\\Users\\arcady\\source\\repos\\cpp-basics\\binary.txt";
 string frombin = "C:\\Users\\arcady\\source\\repos\\cpp-basics\\frombinary.txt";
 
-void to_binary() {
-	ifstream input{ source, ios_base::binary };
-	if (!input) cout << "Can't open file " << source << endl;
-	ofstream output{ outbin, ios_base::binary };
-	if (!output) cout << "Can't open file " << outbin << endl;
-
-	for (char x; input.read(as_bytes(x), sizeof(char)); ) {// note: reading bytes
-		output.write(as_bytes(y), sizeof(char));
+vector<string> split(const string& s){
+	istringstream ss{ s };
+	vector<string> v;
+	string word;
+	while (ss >> word) {
+		v.push_back(word);
+		cout << word << endl;
 	}
-}
-
-
-void from_binary() {
-
-	ifstream input{ outbin };
-	if (!input) cout << "Can't open file " << outbin << endl;
-
-	ofstream from_bin{ frombin, ios_base::binary };
-	if (!from_bin) cout << "Can't open file " << frombin << endl;
-
-	for (char x; input.read(as_bytes(x), sizeof(char));) {
-		from_bin.write(as_bytes(x), sizeof(char));
-	} 
+	return v;
 }
 
 int main() {
 
 	try {
-		to_binary();
-		from_binary();
+		split(source);
 	}
 		
 	catch (exception& e) {
