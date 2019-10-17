14c589e9262a64895f069a5c8e5f889dfb813121
Mcvriez <mcvriez@gmail.com>
2019-10-17 09:58:39 +0300

    ex 12 ch 11 reverse text file

--- chapter_11_customizing_IO.cpp
+++ chapter_11_customizing_IO.cpp
@@ -1,60 +1,33 @@
 #include "std_lib_facilities.h"
 /*
 
-11. Write a function vector<string> split(const string& s, const string& w) that returns a vector of whitespace-
-separated substrings from the argument s, where whitespace is defined as “ordinary whitespace” plus the characters in
-w.
+12. Reverse the order of characters in a text file. For example, asdfghjkl becomes lkjhgfdsa. Warning: There is no really
+good, portable, and efficient way of reading a file backward.
 
 */
 
-const string source = "first !second second seco!nd !thirdth !__!irdthird        still third";
-const string del = "!__!";
-const string del2 = "!";
-const string del3 = "";
+const string source = "C:\\Users\\arcady\\source\\repos\\cpp-basics\\source.log";
+const string reversed = "C:\\Users\\arcady\\source\\repos\\cpp-basics\\reversed.log";
 
-vector<string> split(const string& s, const string& w){
-	istringstream ss{ s };
-	vector<string> v;
-	string word;
-	string buff;
-	char ch;
-
-	while (ss.get(ch)) {
-		if (!isspace(ch))
-			word += ch;
-		else {
-			bool check = true;
-			buff += ch;
-			for (char n : w) {
-				ss.get(ch);
-				buff += ch;
-				if (ch != n) {
-					word += buff;
-					check = false;
-					break;
-				}
-			}
-			buff = "";
-			if (check && word != "") {
-				v.push_back(word); 
-				word = "";
-			}
-		}	
-	}
-	if (word.size()) v.push_back(word);
-	for (string word : v) {
-		cout << word << endl;
-	}
-	cout << v.size() << endl;;
-	return v;
-}
 
 int main() {
 
 	try {
-		split(source, del);
-		split(source, del2);
-		split(source, del3);
+		ifstream ist{ source };
+		if (!ist) cout << "Can't open file";
+
+		ofstream ost{ reversed };
+		if (!ost) cout << "Can't open file";
+
+		char c;
+		vector <char> v;
+
+		while (ist.get(c)) {
+			v.push_back(c);
+		}
+		for (int x = v.size(); x > 0; --x) {
+			ost << v[x - 1];
+		}
 	}
 		
 	catch (exception& e) {
