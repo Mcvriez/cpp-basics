7a6dc8bab56f391709e303ed40a40e17f3d872f9
Mcvriez <mcvriez@gmail.com>
2019-10-19 14:15:32 +0300

    ex 1 ch 12 two rectangles

--- cpp-basics/chapter_12_display_model.cpp
+++ cpp-basics/chapter_12_display_model.cpp
@@ -1,91 +1,37 @@
 #include "Graph.h"
 #include "Simple_window.h"
+/*
+
+1. Draw a rectangle as a Rectangle and as a Polygon. 
+Make the lines of the Polygon red and the lines of the Rectangle blue.
+
+*/
+
+
 
 void routine() {
 
 	Point tl{ 100, 100 }; // top left corner of our window
-	Simple_window win{ tl, 1000, 500, "Canvas" };
-	win.wait_for_button(); // display
+	Simple_window win{ tl, 390, 330, "Canvas" };
+	win.set_label("Excercise 1");
 
-	Axis xa{ Axis::x, Point{20, 400}, 800, 3, "stages" }; // make an Axis
-	win.attach(xa); // attach xa to the window, win
-	win.set_label("Canvas #2"); // relabel the window
-	win.wait_for_button(); // display!
+	Graph_lib::Rectangle rect{ Point { 150, 130 }, 100, 100 }; // top left corner, width, height
+	
+	rect.set_color(Color::dark_cyan);
+	rect.set_style(Line_style(Line_style::solid, 2));
+	
+	Graph_lib::Polygon poly_rect;
+	
+	poly_rect.add(Point{ 100, 50 });	poly_rect.add(Point{ 200, 50 });	
+	poly_rect.add(Point{ 200, 150 }); poly_rect.add(Point{ 100, 150 });
+	poly_rect.set_color(Color::dark_red);
+	poly_rect.set_style(Line_style(Line_style::dashdot, 2));
 
-	Axis ya{ Axis::y, Point{20, 400}, 300, 10, "profit" };
-	ya.set_color(Color::dark_magenta); // choose a color
-	ya.label.set_color(Color::dark_magenta); // choose a color for the text
-	win.attach(ya);
-	win.set_label("Canvas #3");
-	win.wait_for_button(); // display!
-
-	Function sine{ sin, 0, 16, Point{ 20, 300 }, 1000, 50, 50 }; // sine curve
-	win.attach(sine);
-	win.set_label("Canvas #4");
-	win.wait_for_button();
-
-	sine.set_color(Color::dark_blue); // we changed our mind about sine’s color
-	Graph_lib::Polygon poly; // a polygon; a Polygon is a kind of Shape
-	poly.add(Point{ 300,200 }); // three points make a triangle
-	poly.add(Point{ 350,100 });
-	poly.add(Point{ 400,200 });
-	poly.set_color(Color::white);
-	poly.set_style(Line_style::dash);
-	win.attach(poly);
-	win.set_label("Canvas #5");
-	win.wait_for_button();
-
-	Graph_lib::Rectangle r{ Point{150,130}, 50, 150 }; // top left corner, width, height
-	win.attach(r);
-	win.set_label("Canvas #6");
-	win.wait_for_button();
-
-	Closed_polyline poly_rect;
-	poly_rect.add(Point{ 100,50 });
-	poly_rect.add(Point{ 200,50 });
-	poly_rect.add(Point{ 200,100 });
-	poly_rect.add(Point{ 100,100 });
 	win.attach(poly_rect);
-	r.set_fill_color(Color::yellow); // color the inside of the rectangle
-	poly.set_style(Line_style(Line_style::dash, 4));
-	poly_rect.set_style(Line_style(Line_style::dash, 2));
-	poly_rect.set_fill_color(Color::green);
-	win.set_label("Canvas #7");
+	win.attach(rect);
 	win.wait_for_button();
 
-	Text t{ Point{150,150}, "Could be better" };
-	win.attach(t);
-	win.set_label("Canvas #8");
-	win.wait_for_button();
 
-	t.set_font(Font::helvetica_bold);
-	t.set_font_size(202);
-	win.set_label("Canvas #9");
-	win.wait_for_button();
-
-	Image ii{ Point{100,50},"C:\\Users\\arcady\\Pictures\\cut.jpg" }; // 400*212-pixel jpg
-	win.attach(ii);
-	win.set_label("Canvas #10");
-	win.wait_for_button();
-
-	ii.move(200, 300);
-	win.set_label("Canvas #11");
-	win.wait_for_button();
-
-	Circle c{ Point{100,200},50 };
-	Graph_lib::Ellipse e{ Point{100,200}, 75,25 };
-	e.set_color(Color::dark_red);
-	Mark m{ Point{100,200},'x' };
-	ostringstream oss;
-	oss << "screen size: " << x_max() << "*" << y_max()
-	<< "; window size: " << win.x_max() << "*" << win.y_max();
-	Text sizes {Point{100,20},oss.str()};
-	win.attach(c);
-	win.attach(m);
-	win.attach(e);
-	win.attach(sizes);
-	win.set_label("Canvas #12");
-	win.wait_for_button();
 }
 
 
