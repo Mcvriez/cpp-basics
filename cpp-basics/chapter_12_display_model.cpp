1e0e50ae3d4d7cc71d64cada0982361afe8f29e4
Mcvriez <mcvriez@gmail.com>
2019-10-19 14:50:34 +0300

    ex 2 ch 12 rectangle with text

--- cpp-basics/chapter_12_display_model.cpp
+++ cpp-basics/chapter_12_display_model.cpp
@@ -2,8 +2,7 @@
 #include "Simple_window.h"
 /*
 
-1. Draw a rectangle as a Rectangle and as a Polygon. 
-Make the lines of the Polygon red and the lines of the Rectangle blue.
+2. Draw a 100-by-30 Rectangle and place the text “Howdy!” inside it.
 
 */
 
@@ -12,23 +11,19 @@ Make the lines of the Polygon red and the lines of the Rectangle blue.
 void routine() {
 
 	Point tl{ 100, 100 }; // top left corner of our window
-	Simple_window win{ tl, 390, 330, "Canvas" };
+	Simple_window win{ tl, 400, 330, "Canvas" };
 	win.set_label("Excercise 1");
 
-	Graph_lib::Rectangle rect{ Point { 150, 130 }, 100, 100 }; // top left corner, width, height
-	
+	Graph_lib::Rectangle rect{ Point { 150, 150 }, 100, 30 }; // top left corner, width, height
 	rect.set_color(Color::dark_cyan);
 	rect.set_style(Line_style(Line_style::solid, 2));
-	
-	Graph_lib::Polygon poly_rect;
-	
-	poly_rect.add(Point{ 100, 50 });	poly_rect.add(Point{ 200, 50 });	
-	poly_rect.add(Point{ 200, 150 }); poly_rect.add(Point{ 100, 150 });
-	poly_rect.set_color(Color::dark_red);
-	poly_rect.set_style(Line_style(Line_style::dashdot, 2));
-
-	win.attach(poly_rect);
 	win.attach(rect);
+
+	Text t{ Point{ 175, 170}, "Howdy!" };
+	t.set_color(Color::dark_blue);
+	// t.set_font(Font_type::helvetica);
+	win.attach(t);
+
 	win.wait_for_button();
 
 
