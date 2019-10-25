324d8b26c4276d0aae93df60ce892cab4cfef319
Mcvriez <mcvriez@gmail.com>
2019-10-25 09:39:02 +0300

    drill chapter 13 - grid and custom vector

--- cpp-basics/chapter_12_display_model.cpp
+++ cpp-basics/chapter_12_display_model.cpp
@@ -1,91 +1,72 @@
-#define _USE_MATH_DEFINES
 #include "Graph.h"
 #include "Simple_window.h"
-#include<cmath>
 
-/* 
+/*
 
-12. A superellipse is a two-dimensional shape defined by the equation
-Look up superellipse on the web to get a better idea of what such shapes look like. 
+1. Make an 800-by-1000 Simple_window.
+2. Put an 8-by-8 grid on the leftmost 800-by-800 part of that window (so that each square is 100 by 100).
+3. Make the eight squares on the diagonal starting from the top left corner red (use Rectangle).
 
-Write a program that draws “starlike” patterns by connecting points on a superellipse. 
+4. Find a 200-by-200-pixel image (JPEG or GIF) and place three copies of it on the grid (each image covering four
+squares). If you can’t find an image that is exactly 200 by 200, use set_mask() to pick a 200-by-200 section of a larger
+image. Don’t obscure the red squares.
 
-- Take a, b, m, n, and N as arguments. Select N points on the superellipse defined by a, b, m, and n. 
-
-- Make the points equally spaced for some definition of “equal.” 
-
-- Connect each of those N points to one or more other points (if you like you can make the number of points to which to connect a point another
-argument or just use N–1, i.e., all the other points).
-
-13. Find a way to add color to the lines from the previous exercise. Make some lines one color and other lines another color
-or other colors.
+5. Add a 100-by-100 image. Have it move around from square to square when you click the “Next” button. Just put
+wait_for_button() in a loop with some code that picks a new square for your image.
 
 */
 
+const string pic = "C:\\Users\\arcady\\Pictures\\peps.jpg";
+const string pic_s = "C:\\Users\\arcady\\Pictures\\Shark3y.jpg"; 
 
 int main()
-{
-	Simple_window win{ Point { 100, 100 }, 800, 640, "Canvas" };
-	int x0 = 400;
-	int y0 = 320;
-
-	double a;
-	double b;
-	double m;
-	double n;
-	int N;
-	int y2, y1, x;
-
-	vector <Point> points;
-	vector <int> xvector{ -125, -100, -75, -50, 0 , 50, 75, 100, 125 };
-
-	// -------------------- POLIGON -------------------- //
-
-	a = b = 150;
-	n = m = 0.5;
-	for (int x : xvector) {
-		y1 = b * pow(abs(1 - pow(abs((x / a)), n)), 1.0 / m);
-		y2 = y1 * -1;
-		points.push_back(Point{ x + x0, y1 + y0 });
-		points.push_back(Point{ x + x0, y2 + y0 });
-				
+try {
+	Simple_window win{ Point { 100, 100 }, 1000, 800, "Canvas" };
+	Vector_ref <Graph_lib::Rectangle> vr;
+	Lines grid;
+	
+	int size = 100;
+	int max = 800;
+	int count = 8;
+	
+	for (int i = 0; i <= count; ++i) {
+		grid.add(Point {size * i, 0}, Point{ size * i, max});
+		grid.add(Point{ 0, size * i }, Point{ max, size * i });
 	}
-
+	win.attach(grid);
+		
 	
-	Graph_lib::Polygon poly;
-	poly.add(points[0]);
-	poly.add(points[1]);
-	poly.add(points[2]);
-	poly.add(points[3]);
-	poly.set_color(Color::dark_green);
-	poly.set_style(Line_style(Line_style::dash, 3));
-
-	Graph_lib::Polygon poly1;
-	poly1.add(points[4]);
-	poly1.add(points[5]);
-	poly1.add(points[6]);
-	poly1.add(points[7]);
-	poly1.set_color(Color::dark_red);
-	poly1.set_style(Line_style(Line_style::dot, 4));
+	for (int i = 0; i < count; ++i){
+		vr.push_back(new Graph_lib::Rectangle{ Point{i * size, i * size }, size, size });
+		vr[(vr.size() - 1)].set_fill_color(Color::dark_red);
+		win.attach(vr[(vr.size() - 1)]);
+		}
+	Graph_lib::Image pic1({ 300, 500 }, pic);
+	Graph_lib::Image pic2({ 500, 300 }, pic);
+	Graph_lib::Image pic3({ 100, 300 }, pic);
 	
-	Graph_lib::Polygon poly2;
-	poly2.add(points[8]);
-	poly2.add(points[9]);
-	poly2.add(points[10]);
-	poly2.add(points[11]);
-	poly2.set_color(Color::dark_cyan);
-	poly2.set_style(Line_style(Line_style::dash, 3));
-	
-	Graph_lib::Polygon poly3;
-	poly3.add(points[12]);
-	poly3.add(points[13]);
-	poly3.add(points[14]);
-	poly3.add(points[15]);
-	poly3.set_style(Line_style(Line_style::dashdot, 2));
+	pic1.set_mask(Point{ 400, 400 }, 200, 200);
+	pic2.set_mask(Point{ 600, 200 }, 200, 200);
+	pic3.set_mask(Point{ 100, 500 }, 200, 200);
+	win.attach(pic1); win.attach(pic2); win.attach(pic3);
+	   	  
+	while (true) {
+		int x = rand() % 8;
+		int y = rand() % 8;
+		cout << x << ' ' << y << endl;
+		Graph_lib::Image pic4({ x * 100, y * 100 }, pic_s);
+		pic4.set_mask(Point{ 100, 100 }, 100, 100);
+		win.attach(pic4);
+		win.wait_for_button();
+		win.detach(pic4);
+	}
+}
 
-	win.attach(poly);
-	win.attach(poly1);
-	win.attach(poly2);
-	win.attach(poly3);
-	win.wait_for_button();
+catch (exception& e) {
+	cerr << "exception: " << e.what() << endl;
+	keep_window_open();
+}
+catch (...) {
+	cerr << "exception\n";
+	keep_window_open();
 }
