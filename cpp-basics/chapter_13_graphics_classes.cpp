e9a203844cef03b692c46bd5ee185205438bc1b2
Mcvriez <mcvriez@gmail.com>
2019-11-05 07:47:31 +0200

    ex 16-17 ch 13 hexagons

--- cpp-basics/chapter_13_graphics_classes.cpp
+++ cpp-basics/chapter_13_graphics_classes.cpp
@@ -4,7 +4,8 @@
 /*
 
 For each “define a class” exercise, display a couple of objects of the class to demonstrate that they work.
-15. “Tile” a window with small right triangles.
+16. Do the previous exercise, but with hexagons.
+17. Do the previous exercise, but using hexagons of a few different colors.
 
 */
 
@@ -13,42 +14,26 @@ For each “define a class” exercise, display a couple of objects of the class to 
 int main()
 try {
 
-	Point tl(200, 50);
-	Point pzero(30, 50);
-	int winx = 1200;
-	int winy = 800;
+	Simple_window win{ Point { 100, 100 }, 1000, 900, "Canvas" };
+	int rad = 50;
 
-	Simple_window win(tl, winx, winy, "My window");
-	Vector_ref<Graph_lib::Right_triangle> vrt;
+	Vector_ref<Graph_lib::Regular_hexagon> vr;
 
-	int size = 35;
-	int step = size * sqrt(2);
+	int base_x = 0;
+	int base_y = 0;
+	for (int i = 0; i < 15; ++i)
+		for (int j = 0; j < 20; ++j) {
+			if (j % 2 > 0 || j == 1) { base_x = 56; }
+			vr.push_back(new Regular_hexagon(Point{ base_x + i * 87, base_y + 75 * j }, rad));
+			vr[vr.size() - 1].set_fill_color(rand() % 256);
+			vr[vr.size() - 1].set_color(Color::invisible);
+			win.attach(vr[vr.size() - 1]);
+			base_x = 100;
 
-	for (int i = 0; i < winy / (step + 1); ++i)
-		for (int j = 0; j < winx / (step + 1); ++j)
-		{
-			vrt.push_back(new Graph_lib::Right_triangle(Point(pzero.x + j * step, pzero.y + i * step), size, size, 45));
-			vrt[vrt.size() - 1].set_fill_color(rand() % 255);
-			vrt[vrt.size() - 1].set_color(Color::invisible);
-			win.attach(vrt[vrt.size() - 1]);
-			vrt.push_back(new Graph_lib::Right_triangle(Point(pzero.x + j * step, pzero.y + i * step), size, size, 135));
-			vrt[vrt.size() - 1].set_fill_color(rand() % 255);
-			vrt[vrt.size() - 1].set_color(Color::invisible);
-			win.attach(vrt[vrt.size() - 1]);
-			vrt.push_back(new Graph_lib::Right_triangle(Point(pzero.x + j * step, pzero.y + i * step), size, size, -45));
-			vrt[vrt.size() - 1].set_fill_color(rand() % 255);
-			vrt[vrt.size() - 1].set_color(Color::invisible);
-			win.attach(vrt[vrt.size() - 1]);
-			vrt.push_back(new Graph_lib::Right_triangle(Point(pzero.x + j * step, pzero.y + i * step), size, size, -135));
-			vrt[vrt.size() - 1].set_fill_color(rand() % 255);
-			vrt[vrt.size() - 1].set_color(Color::invisible);
-			win.attach(vrt[vrt.size() - 1]);
 		}
-
-
 	win.wait_for_button();
-
 }
+
 catch (exception& e) {
 	cerr << "exception: " << e.what() << endl;
 	keep_window_open();
