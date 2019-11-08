191e0af3ac6ea0252e483da89c8a3678e289b758
Mcvriez <mcvriez@gmail.com>
2019-11-08 14:00:38 +0200

    ex 6 h 14 - striped circle

--- cpp-basics/chapter_14_graphics_class_design.cpp
+++ cpp-basics/chapter_14_graphics_class_design.cpp
@@ -4,24 +4,23 @@
 
 /*
 
-5. Define a Striped_rectangle where instead of fill, the rectangle is “filled” by drawing one-pixel-wide horizontal lines
-across the inside of the rectangle (say, draw every second line like that). 
-You may have to play with the width of lines and the line spacing to get a pattern you like.
+6. Define a Striped_circle using the technique from Striped_rectangle.
 
 */
 
 
-class Striped_rectangle : public Rectangle {
+class Striped_circle : public Circle {
 public:
-	using Rectangle::Rectangle;
+	using Circle::Circle;
 	void draw_lines() const {
-		if (fill_color().visibility()) {	
+		if (fill_color().visibility()) {
 			fl_color(fill_color().as_int());
 			fl_line_style(0, pixel_step);
-			for (int i =0; i * pixel_step < w; ++i) {
+			for (int i =0; i * pixel_step < 2 * r; ++i) {
+				int y = sqrt(pow(r, 2) - pow(r - i * pixel_step, 2));
 				if (i % 2 != 0) {
-					cout << i << point(0).y << i << point(0).y + h << endl;
-					fl_line(point(0).x + i * pixel_step, point(0).y, point(0).x + i * pixel_step, point(0).y + h);
+					cout << i << point(0).y << i << point(0).y + 2 * r << endl;
+					fl_line(point(0).x + i * pixel_step, point(0).y + r + y, point(0).x + i * pixel_step, point(0).y + r - y);
 				}
 			}
 			fl_color(color().as_int());	
@@ -29,7 +28,7 @@ public:
 		if (color().visibility()) {
 			fl_line_style(color().as_int(), pixel_step);
 			fl_color(color().as_int());
-			fl_rect(point(0).x, point(0).y, w, h);
+			fl_arc(point(0).x, point(0).y, r + r, r + r, 0, 360);
 		}
 	}
 	void set_step(int x) {
@@ -48,7 +47,7 @@ try {
 	Point start{ 300, 300 };
 	Simple_window win{ Point { 100, 100 }, 1000, 900, "Canvas" };
 
-	Striped_rectangle rect { start , 300, 150 };
+	Striped_circle rect { start , 100 };
 	
 	rect.set_fill_color(Color::dark_cyan);
 	win.attach(rect);
@@ -56,7 +55,7 @@ try {
 
 	rect.set_fill_color(Color::dark_magenta);
 	rect.move(100, 100);
-	rect.set_step(15);
+	rect.set_step(10);
 	win.wait_for_button();
 
 
