9920495a66ea284385f9ad248da3f919c6b81fb1
wq <mcvriez@gmail.com>
2020-02-24 22:43:30 +0200

    ex 16 ch 14 Controller super class

--- cpp-basics/chapter_14_graphics_class_design.cpp
+++ cpp-basics/chapter_14_graphics_class_design.cpp
@@ -1,113 +1,134 @@
-//#include "Graph.h"
-//#include "Simple_window.h"
-//#include "std_lib_facilities.h"
-#include<iostream>
-#include<vector>
-#include<list>
-using namespace std;
+#include "Graph.h"
+#include "Simple_window.h"
+#include "std_lib_facilities.h"
+#include <cstdlib>
+
+//#include<iostream>
+//#include<vector>
+//#include<list>
+//using namespace std;
+
 
 /*
 
-15. Most class hierarchies have nothing to do with graphics.
-Define a class Iterator with a pure virtual function next() that returns a double* (see Chapter 17).
+16. Define a class Controller with four virtual functions on(), off(), set_level(int), and show().
+Derive at least two classes from Controller.
 
-Now derive Vector_iterator and List_iterator from Iterator so that next()
- for a Vector_iterator yields a pointer to the next element of a vector<double> and
- List_iterator does the same for a list<double>.
+One should be a simple test class where show() prints out whether the class is set to on or off and what is the current level.
 
-You initialize a Vector_iterator with a vector<double> and the first call of next() yields a pointer
-to its first element, if any. If there is no next element, return 0.
-Test this by using a function void print(Iterator&) to print the elements of a vector<double> and a list<double>.
+The second derived class should somehow control the line color of a Shape;
+the exact meaning of “level” is up to you.
+
+Try to find a third “thing” to control with such a Controller class.
 
 */
 
-// UNFINISHED - need to fix after chapter 17
-
-std::vector<double> vd {12, 888, 43, 131, 4534.3, 777};
-std::list<double> ld {11.101, 12, 13, 14, 2223, 16, 17.1717, 122.211};
-
-class Iteratorx{ ;
-public:
-    virtual double* next() { double *x = nullptr; return x;}
+class Controller {
+    virtual void on() = 0;
+    virtual void off() = 0;
+    virtual void set_level(int x) = 0;
+    virtual void show() = 0;
 };
-void print(Iteratorx& it) {
-    double* res = it.next();
-    if (res != nullptr) cout << res << " ~~~~> " << *res << endl;
-    else cout << "<nullptr>" << endl;
-}
-class Vector_iterator : public Iteratorx {
+class Test : Controller {
 public:
-    explicit Vector_iterator(std::vector<double>& va);
-    double* next() override {
-        cout << position << " ";
-        if (position > size - 1 || size < 1) return nullptr;
-        if (position > 0) pDouble += 1;
-        ++position;
-        cout << "???" << endl;
-        return pDouble;
+    Test(int lev, bool st): level {lev}, turned_on {st} {if (lev < 0) level = 0;};
+    explicit Test(int lev): level {lev}, turned_on {false} {if (lev < 0) level = 0;};
+    Test(): level {0}, turned_on {false} {};
+    void set_level(int x) override { if (x >= 0) level = x; else cout << "Attempt to assign negative level: " << x << endl;}
+    void on() override {turned_on = true;}
+    void off() override {turned_on = false;}
+    void show() override {
+        cout << "Test class object is turned ";
+        turned_on ? cout << "on" << endl : cout << "off" << endl;
+        cout << "Test class object level is: " << level << endl;
     }
 private:
-    double* pDouble;
-    int position;
-    int size;
-
+    int level;
+    bool turned_on;
 };
-
-Vector_iterator::Vector_iterator(std::vector<double> &va) : position {0} {
-    size = va.size();
-    cout  << size << endl;
-    if (size > 0) {
-        pDouble= &va[0];
-    }
-    cout << pDouble << " ----> " << *pDouble << endl;
-    cout << "---------" << endl;
-}
-
-class List_iterator : public Iteratorx {
+class ShapeController : Controller {
 public:
-    explicit List_iterator(std::list<double>& la) : position {0} {
-        size = la.size();
-        cout  << size << endl;
-        if (size > 0) {
-            pDouble= &la.front();
+    explicit ShapeController(Shape& sh): shape {sh}, color {sh.color()}, old_color {sh.color()}, turned_on {false} {};
+    void set_level(int x) override {
+        if (x >= 0) color = x;
+        else cout << "Attempt to assign negative level: " << x << endl;
+    }
+    void on() override {turned_on = true;}
+    void off() override {turned_on = false;}
+    void show() override {
+        if (turned_on) {shape.set_color(color);} else shape.set_color(old_color);;
+    }
+private:
+    Shape& shape;
+    Graph_lib::Color color;
+    Graph_lib::Color old_color;
+    bool turned_on;
+};
+class SoundController : Controller {
+public:
+    SoundController (): turned_on {false}, level{15}, new_level {0} {};
+    void set_level(int x) override { if (x < 100 && x > -100) new_level =x;}
+    void on() override {turned_on = true;}
+    void off() override {turned_on = false; show();}
+    void show() override {
+        if (turned_on) {
+            string scom = "pactl set-sink-volume 1 " + to_string(new_level) + "%";
+            const char* com = scom.c_str();
+            cout << com << endl;
+            system(com);}
+        else {
+            string scom = "pactl set-sink-volume 1 " + to_string(level) + "%";
+            const char* com = scom.c_str();
+            cout << com << endl;
+            system(com);
         }
-    };
-
-    double* next() override {
-        if (position >= size || size < 1) return nullptr;
-        if (position > 0) pDouble += 4;
-        ++position;
-        return pDouble;
     }
 private:
-    double* pDouble;
-    int position;
-    int size;
+    int level;
+    int new_level;
+    bool turned_on;
 };
-
-
 
 int main()
 try {
-    List_iterator test_v (ld);
-    print(test_v);
-    print(test_v);
-    print(test_v);
-    print(test_v);
-    print(test_v);
-    print(test_v);
-    print(test_v);
-    print(test_v);
-    print(test_v);
-    print(test_v);
-    print(test_v);
-    print(test_v);
-    print(test_v);
-    print(test_v);
-    print(test_v);
+
+    Point tl(500,500);
+    Point tl2(300,300);
+    Simple_window win(tl,2400,1500,"My window");
+
+
+    Rectangle rec {tl2, 700, 700};
+    rec.set_style(Line_style(Line_style::dash, 10)); win.attach(rec);
+    win.wait_for_button();
+
+    ShapeController cont (rec); cont.on(); cont.set_level(199); cont.show();
+    win.wait_for_button();
+
+    cont.set_level(99); cont.off(); cont.show();
+    win.wait_for_button();
+
+    Test t1 {};
+    t1.show(); t1.set_level(15); t1.on(); t1.set_level(-14); t1.show();
+
+    SoundController sc;
+    win.wait_for_button();
+
+    sc.on();sc.set_level(10);sc.show();
+    win.wait_for_button();
+
+    sc.on();sc.set_level(20);sc.show();
+    win.wait_for_button();
+
+    sc.on();sc.set_level(-20);sc.show();
+    win.wait_for_button();
+
+    sc.on();sc.off();
+    win.wait_for_button();
+
+
+
 
 }
-
 catch (exception& e) {
     cerr << "exception: " << e.what() << endl;
 }
