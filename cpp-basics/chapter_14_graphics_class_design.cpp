44973a1b709b2144d8209f1e90ff3e4dbc1311d4
wq <mcvriez@gmail.com>
2020-02-26 15:38:14 +0200

    ex 5 ch 15 Leibniz function

--- cpp-basics/chapter_14_graphics_class_design.cpp
+++ cpp-basics/chapter_14_graphics_class_design.cpp
@@ -4,13 +4,8 @@
 
 /*
 
-Graph
- a sine (sin()),
- a cosine (cos()),
- the sum of those (sin(x) + cos(x)),
- and the sum of the squares of those (sin(x)*sin(x)+cos(x)*cos(x)) on a single graph.
-
-Do provide axes and labels.
+5. “Animate” (as in §15.5) the series 1 – 1/3 + 1/5 – 1/7 + 1/9 – 1/11 + . . . .
+It is known as Leibniz’s series and converges to pi/4.
 
 */
 
@@ -20,6 +15,20 @@ constexpr int ylength = ymax - yoffset - yspace; constexpr int notches = 20;
 
 constexpr int r_min = -5; constexpr int r_max = 5; constexpr int n_points = 400;
 constexpr int x_scale = 80; constexpr int y_scale = 80;
+
+double precision = 0;
+double leibniz_f(double x) {
+    double f = 1;
+    int del = 3;
+    int sign = -1;
+    for (int i = 0; i < precision; ++i) {
+        f += (double(1) / del) * sign;
+        del += 2;
+        sign *= -1;
+    }
+    return f;
+}
+
 
 int main()
 try {
@@ -36,31 +45,17 @@ try {
     win.attach(x_axis);
     win.attach(y_axis);
 
+    while (precision < 100 ){
+        Function cos_f {leibniz_f, r_min, r_max, pmidle, n_points, x_scale, y_scale};
+        cout << leibniz_f(100) * 4 << " " << precision <<  endl;
+        cos_f.set_color(Color::dark_blue);
+        cos_f.set_style(Line_style(Line_style::solid, 2));
+        win.attach(cos_f);
 
-    Function cos_f {cos, r_min, r_max, pmidle, n_points, x_scale, y_scale};
-    cos_f.set_color(Color::dark_blue);
-
-    Function sin_f {sin, r_min, r_max, pmidle, n_points, x_scale, y_scale};
-    sin_f.set_color(Color::dark_green);
-
-    Function sum_f {[](double x) {return cos(x) + sin(x);}, r_min, r_max, pmidle, n_points, x_scale, y_scale};
-    sum_f.set_color(Color::dark_yellow);
-
-    Function sum_sq_f {[](double x) {return cos(x) * cos(x) + sin(x) * sin(x);}, r_min, r_max, pmidle, n_points, x_scale, y_scale};
-    sum_sq_f.set_color(Color::dark_magenta);
-
-    win.attach(cos_f);
-    win.attach(sin_f);
-    win.attach(sum_f);
-    win.attach(sum_sq_f);
-
-    sum_f.set_style(Line_style(Line_style::solid, 4));
-    sum_sq_f.set_style(Line_style(Line_style::solid, 2));
-    sin_f.set_style(Line_style(Line_style::solid, 2));
-    cos_f.set_style(Line_style(Line_style::solid, 2));
-
-
-    win.wait_for_button();
+        win.wait_for_button();
+        win.detach(cos_f);
+        ++precision;
+    }
 }
 
 catch (exception& e) {
