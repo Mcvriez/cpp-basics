88dd4506f85437ef2b242b59ff73e076117ffda4
wq <mcvriez@gmail.com>
2020-02-26 19:14:24 +0200

    ex 7 ch 15 Bar class with flavor

--- cpp-basics/chapter_14_graphics_class_design.cpp
+++ cpp-basics/chapter_14_graphics_class_design.cpp
@@ -4,9 +4,7 @@
 
 /*
 
-6. Design and implement a bar graph class.
-Its basic data is a vector<double> holding N values,
-and each value should be represented by a “bar” that is a rectangle where the height represents the value.
+7. Elaborate the bar graph class to allow labeling of the graph itself and its individual bars. Allow the use of color.
 
 */
 
@@ -16,40 +14,50 @@ constexpr int ylength = ymax - yoffset - yspace; constexpr int notches = 20;
 constexpr int r_min = -5; constexpr int r_max = 5; constexpr int n_points = 400;
 constexpr int x_scale = 80; constexpr int y_scale = 80;
 
-class Bar: public Shape {
+struct Bar {
+    explicit Bar (double v, const string& lab = "None", int col = 0) : value {v}, color {col}, label {lab} {}
+    double value;
+    int color;
+    string label;
+};
+
+class Bars: public Shape {
     Point root;
     int scale;
     int width;
     int step;
-    vector<double> values;
+    Vector_ref <Bar> bars;
+
 public:
     void draw_lines() const override{
         if (fill_color().visibility()) {
             fl_color(fill_color().as_int());
-            for (int i = 0; i < values.size(); ++i) {
+            for (int i = 0; i < bars.size(); ++i) {
                 int x = root.x + (i * (width + step) + step) * scale;
-                int y = int(root.y - scale * values[i]);
-                fl_rectf(x, y, width * scale, int(scale * values[i]));
+                int y = int(root.y - scale * bars[i].value);
+                if (bars[i].color != 0) fl_color(bars[i].color);
+                fl_rectf(x, y, width * scale, int(scale * bars[i].value));
             }
             fl_color(color().as_int());
         }
         if (color().visibility()) {
             fl_color(color().as_int());
-            for (int i = 0; i < values.size(); ++i) {
+            for (int i = 0; i < bars.size(); ++i) {
                 int x = root.x + (i * (width + step) + step) * scale;
-                int y = int(root.y - scale * values[i]);
-                fl_rect(x, y, width * scale, int(scale * values[i]));
+                int y = int(root.y - scale * bars[i].value);
+                fl_rect(x, y, width * scale, int(scale * bars[i].value));
+                if (bars[i].label != "None") {
+                    fl_draw(bars[i].label.c_str(), x + width * scale / 2 - bars[i].label.size()*3, y - 25);
+                }
             }
         }
     }
-
-    explicit Bar (Point r = Point {0,0}, const vector <double>& d = {}, int sc = 10, int w = 10, int s = 5):
-    root {r}, width {w}, step {s}, scale {sc} {
-        if (!d.empty()) {
-            for (int i = 0; i < d.size(); i++)
-                values.push_back(d[i]);
-        }
-    }
+    explicit Bars (
+            const Vector_ref <Bar>& vr,
+            Point r = Point {0, 0},
+            int sc = 10,
+            int w = 10,
+            int s = 5) : root {r}, width {w}, step {s}, scale {sc}, bars {vr} { }
 };
 
 int main()
@@ -66,8 +74,14 @@ try {
     win.attach(x_axis);
     win.attach(y_axis);
 
-    vector <double> values {12, 52, 11, 4, 23};
-    Bar new_bar (root, values, 10);
+    Vector_ref <Bar> some_bars;
+    some_bars.push_back(new Bar{15, "Start", 38});
+    some_bars.push_back(new Bar{23, "None", 123});
+    some_bars.push_back(new Bar{55, "None", 228});
+    some_bars.push_back(new Bar{43, "Very loong name", 67});
+    some_bars.push_back(new Bar{9, "Finish", 87});
+
+    Bars new_bar (some_bars, root, 15);
     new_bar.set_fill_color(Color::dark_green);
     new_bar.set_style(Line_style(Line_style::dash, 3));
     win.attach(new_bar);
