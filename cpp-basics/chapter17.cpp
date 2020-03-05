11fef8f956628916da63f32a09bd091abd24b991
wq <mcvriez@gmail.com>
2020-03-05 21:00:37 +0200

    ex 8 chapter 17 cin to string

--- cpp-basics/chapter17.cpp
+++ cpp-basics/chapter17.cpp
@@ -2,68 +2,36 @@
 
 /*
 
-7. Write a program that reads characters from cin into an array that you allocate on the free store.
-Read individual characters until an exclamation mark (!) is entered.
-Do not use a std::string. Do not worry about memory exhaustion.
+8. Do exercise 7 again, but this time read into a std::string rather than to memory you put on the free store
+(string knows how to use the free store for you).
 
 */
 
-vector<char*> expired;
 
-char* temp (char* prev_arr_p, int size, char last){
-    char* temp_arr = new char[size + 1];
-    int i = 0;
-    if (prev_arr_p) {
-        while(prev_arr_p[i])
-            {temp_arr[i] = prev_arr_p[i]; ++i;}
-
-        temp_arr[i] = last;
-        temp_arr[i + 1] = char(0);
-        expired.push_back(prev_arr_p);
-        delete[] prev_arr_p;
-    }
-    else temp_arr[0] = last;
-    return temp_arr;
-}
-
-
-char* read_from(istream &is) {
+string read_from_s(istream &is) {
     cout << "Enter characters, '!' to escape" << endl;
     char ch;
     int i = 0;
-    char* pch = nullptr;
+    string arr;
     while(is.get(ch) && ch != '!'){
         ++i;
-        pch = temp(pch, i, ch);
+        arr.push_back(ch);
     }
-    return pch;
+    return arr;
 }
-
 
 int main()
 try
 {
-    char* ret = read_from(cin);
+    string ret = read_from_s(cin);
     int i = 0;
     while(ret[i]) {cout << ret[i] << "~"; ++i;}
     cout << endl << "-----------" << endl;
 
-    // checking garbage
-    for (char* x: expired){
-        int j = 0;
-        if (x) {
-            while(x[j]) {
-            cout << x[j];
-            ++j;
-            }
-            cout << endl;
-        }
-    }
 }
 catch (exception& e) {
     cerr << "exception: " << e.what() << endl;
-    cout << expired.size();
-}
+    }
 catch (...) {
     cerr << "exception\n";
 }
