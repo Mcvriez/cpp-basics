bc2e275b814497a3f64e101871cc3df5b0b760e9
wq <mcvriez@gmail.com>
2020-03-05 14:21:10 +0200

    ex 4 chapter 17 C-string in the free store

--- cpp-basics/chapter17.cpp
+++ cpp-basics/chapter17.cpp
@@ -1,29 +1,27 @@
 #include "lib/std_lib_facilities.h"
 
 /*
- 3.
- Write a function, void to_lower(char* s), that replaces all uppercase characters in the C-style string s with their lowercase equivalents.
- For example, Hello, World! becomes hello, world!.
- Do not use any standard library functions.
- A C-style string is a zero-terminated array of characters, so if you find a char with the value 0 you are at the end.
+
+4. Write a function, char* strdup(const char*), that copies a C-style string into memory it allocates on the free store.
+Do not use any standard library functions.
+
 */
 
-void to_lower(char* s) {
+char* strdup(const char* pcc) {
     int i = 0;
-    while(s[i]){
-        if (int(s[i]) > 64 && int(s[i]) < 91){
-            int value = int(s[i]) + 32;
-            s[i] = char(value);
-        }
-        ++i;
-    }
+    while(pcc[i]){++i;}
+    char* res = new char[i]; i = 0;
+    while (pcc[i]){ res[i] = pcc[i]; ++i; }
+    res[i] = char(0); // 0 terminator
+    return res;
 }
 
 int main()
 try {
-    char mys[]{ "Hello, World! AZ 0-~=``//" };
-    to_lower(mys);
-    cout << mys;
+    string s = "12345";
+    char* pcn = strdup(s.c_str());
+    cout << pcn << "~" << pcn[0] << pcn[4] << pcn[5]<<s.c_str()[5] << endl;
+    cout << pcn << "~" << s.c_str()[0] << s.c_str()[4] << s.c_str()[5] << s.c_str()[6];
 }
 
 catch (exception& e) {
