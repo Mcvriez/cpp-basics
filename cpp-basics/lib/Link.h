0a59dd7cf59a25f6c9b2cb749bd4012d19c7e4a2
wq <mcvriez@gmail.com>
2020-03-09 21:48:20 +0200

    ex 13 chapter 17 linked operations (list of gods)

--- cpp-basics/lib/Link.h
+++ cpp-basics/lib/Link.h
@@ -11,23 +11,41 @@ using namespace std;
 //• find: find a Link with a given value
 //• advance: get the nth successor
 
+//1 Modify the Link class from §17.10.1 to hold a value of a struct God.
+//struct God should have members of type string: name, mythology, vehicle, and weapon.
+//For example, God{"Zeus", "Greek", "", "lightning"} and God{"Odin", "Norse", "Eight-legged flying horse called Sleipner", "Spear called Gungnir"}.
+
+
+struct God {
+    God (const string& n, const string& m, const string& v, const string& w)
+    : name {n}, mythology{m}, vehicle {v}, weapon{w} {};
+    string name;
+    string mythology;
+    string vehicle;
+    string weapon;
+};
+
 class Link {
 public:
-    string value;
-    explicit Link(string  v, Link* p = nullptr, Link* s = nullptr) : value{std::move(v)}, prev{p}, succ{s} { }
+    const God description;
+
+    explicit Link(God god, Link* p = nullptr, Link* s = nullptr) : description {god}, prev{p}, succ{s} { }
 
     Link* erase();
     const Link* find(const string& s) const;
-
     Link* insert(Link* n) ;
     Link* add(Link* n) ;
     Link* find(const string& s);
     Link* advance(int n) const;
+    Link* add_ordered(Link* n);
     Link* next() const { return succ; }
     Link* previous() const { return prev; }
+    Link* pop();
 private:
     Link* prev;
     Link* succ;
+
+
 };
 
 
