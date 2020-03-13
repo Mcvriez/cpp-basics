52c2a39f53b29bc24ad8f7c69bd47a9746613c04
wq <mcvriez@gmail.com>
2020-03-13 10:49:49 +0200

    ex 14 chapter 17 singly-linked list

--- cpp-basics/chapter17.cpp
+++ cpp-basics/chapter17.cpp
@@ -1,79 +1,62 @@
 #include "lib/Link.h"
 
 /*
-    13.
 
-    1 Modify the Link class from §17.10.1 to hold a value of a struct God.
-    struct God should have members of type string: name, mythology, vehicle, and weapon.
-    For example, God {"Zeus", "Greek", "", "lightning"} and God{"Odin", "Norse", "Eight-legged flying horse called Sleipner", "Spear called Gungnir"}.
+14. Could the “list of gods” example from §17.10.1 have been written using a singly-linked list;
+that is, could we have left the prev member out of Link?
+Why might we want to do that?
 
-    2 Write a print_all() function that lists gods with their attributes one per line.
-
-    3 Add a member function add_ordered() that places its new element in its correct lexicographical position.
-
-    4 Using the Links with the values of type God, make a list of gods from three mythologies;
-    then move the elements (gods) from that list to three lexicographically ordered lists — one for each mythology.
+For what kind of examples would it make sense to use a singly-linked list?
+Re-implement that example using only a singly-linked list.
 
 */
 
-void const print_all (Link* p)
+
+void print_all(Link* p)
 {
-    cout << "------------\n";
-    while(p->previous()) p = p->previous();
+    cout << "{ ";
     while (p) {
-        if (p->description.name != "~")
-            cout << p -> description.name << ": "<< p -> description.weapon << "; "<< p -> description.vehicle << endl;
+        cout << p -> value;
         p = p -> next();
+        if (p) cout << ", ";
+    }
+    cout << " }\n";
+}
+
+void gods(){
+    Link* norse_gods = new Link{"Thor"};
+    norse_gods = norse_gods->insert(new Link{"Odin"});
+    norse_gods = norse_gods->insert(new Link{"Zeus"});
+    norse_gods = norse_gods->insert(new Link{"Freia"});
+
+    Link* greek_gods = new Link{"Hera"};
+    greek_gods = greek_gods->insert(new Link{"Athena"});
+    greek_gods = greek_gods->insert(new Link{"Mars"});
+    greek_gods->add(new Link{"Hermes"});
+    greek_gods = greek_gods->insert(new Link{"Poseidon"});
+
+    Link* p2 = norse_gods->find("Zeus");
+    if (p2) {
+        if (p2==norse_gods) norse_gods = p2->next(); // re-binding norse_gods to the first element if Zeus was first
+        p2->erase();
+        greek_gods = greek_gods->insert(p2);
     }
 
+    Link* p = greek_gods->find("Mars");
+    if (p) p->value = "Ares";
+
+
+
+    print_all(norse_gods);
+    print_all(greek_gods);
 }
-
-Link* gods(){
-    Link*  gods = new Link {God {"Zeus", "Greek", "", "lightning"}};
-
-    gods = gods->add_ordered (new Link {God{"Odin", "Norse", "Eight-legged flying horse called Sleipner", "Spear called Gungnir"}});
-    gods = gods->add_ordered (new Link {God{"Thor", "Norse", "Chariot drawn by goats", "Mjöllnir"}});
-    gods = gods->add_ordered (new Link {God{"Vídar", "Norse", "", "Unnamed sword"}});
-    gods = gods->add_ordered (new Link {God{"Loki", "Norse", "", "Deception"}});
-    gods = gods->add_ordered (new Link {God {"Poseidon", "Greek", "Chariot drawn by horses", "Trident"}});
-    gods = gods->add_ordered (new Link {God {"Apollo", "Greek", "Chariot pulled by swans", "Bow and arrows"}});
-    gods = gods->add_ordered (new Link {God {"Athena", "Greek", "", "Spear and aegis"}});
-    gods = gods->add_ordered (new Link {God {"Huitzilopochtli", "Aztec", "", "Xiuhcoatl, turquoise snake"}});
-    gods = gods->add_ordered (new Link {God {"Quetzalcoatl", "Aztec", "", "Lethal darts"}});
-    gods = gods->add_ordered (new Link {God {"Tezcatlipoca", "Aztec", "", "Spear and shield"}});
-    gods = gods->add_ordered(new Link {God {"Yogg Saron", "Old gods", "", "The Beast With a Thousand Maws"}});
-    gods = gods->add_ordered(new Link {God {"C'Tun", "Old gods", "", "Madness and chaos"}});
-    gods = gods->add_ordered(new Link {God {"N'Zoth", "Old gods", "", "Corruption and nightmares"}});
-    return gods;
-}
-
-
 
 int main()
 try
 {
-    Link* god_list = gods();
-    //print_all(god_list);
-    Link* aztec_gods = new Link {God {"~", "~", "", "~"}};
-    Link* greek_gods = new Link {God {"~", "~", "", "~"}};
-    Link* norse_gods = new Link {God {"~", "~", "", "~"}};
-    Link* old_gods = new Link {God {"~", "~", "", "~"}};
 
-    while(god_list->previous()) god_list = god_list->previous();
-    while(god_list){
-        Link* p = god_list;
-        god_list = god_list->erase();
-        cout << p->description.name << " " << p << endl;
-        if (p && p->description.mythology == "Greek") greek_gods->add_ordered(p);
-        if (p && p->description.mythology == "Aztec") aztec_gods->add_ordered(p);
-        if (p && p->description.mythology == "Norse") norse_gods->add_ordered(p);
-        if (p && p->description.mythology == "Old gods") old_gods->add_ordered(p);
-    }
 
-    print_all(old_gods);
-    print_all(norse_gods);
-    print_all(greek_gods);
-    print_all(aztec_gods);
+    gods();
 
 
 }
@@ -84,4 +67,3 @@ catch (exception& e)
 catch (...) {
     cerr << "exception\n";
 }
-
--- cpp-basics/lib/Link.cpp
+++ cpp-basics/lib/Link.cpp
@@ -3,7 +3,7 @@
 const Link *Link::find(const string &s) const {
     const Link* p  = this;
     while(p){
-        if (p->description.name == s) return p;
+        if (p->value == s) return p;
         p = p->next();
     }
     return nullptr;
@@ -11,7 +11,7 @@ const Link *Link::find(const string &s) const {
 Link* Link::find(const string &s) {
     Link* p  = this;
     while(p){
-        if (p->description.name == s) return p;
+        if (p->value == s) return p;
         p = p->next();
     }
     return nullptr;
@@ -25,78 +25,28 @@ Link* Link::advance(int n) const {
             p = next();
         }
     }
-    else if (n < 0) {
-        while (n++) {
-            if (previous() == nullptr) return nullptr;
-            p = previous();
-        }
-    }
+    else if (n < 0)  p = const_cast<Link *>(this);
     return p;
 }
 Link* Link::insert(Link* n) {
     if (n == nullptr) return this;
     if (this == nullptr) {cout << "nullptr -> Link::insert\n"; return this;}
     n->succ = this;
-    if (prev) prev->succ = n;
-    n->prev = prev;
-    prev = n;
-    //cout << this->description.name << " inserts " << n->description.name << endl;
     return n;
 }
 Link* Link::erase()
 {
     Link* p = this;
     if (p == nullptr) {cout << "nullptr -> Link::erase\n"; return nullptr;}
-    if (p->succ) p->succ->prev = p->prev;
-    if (p->prev) p->prev->succ = p->succ;
     return p->succ;
 }
 
-Link* Link::pop()
-{
+Link *Link::add(Link* n) {
     Link* p = this;
-    if (p == nullptr) {cout << "nullptr -> Link::erase\n"; return nullptr;}
-    if (p->succ) p->succ->prev = p->prev;
-    if (p->prev) p->prev->succ = p->succ;
-
-    p->prev = nullptr; p->succ = nullptr;
-    return p->succ;
-}
-
-Link* Link::add(Link *n) {
-    if (n == nullptr) return this;
-    if (this == nullptr) {cout << "nullptr -> Link::add\n"; return this;}
-    n->prev = this;
-    if (succ) {
-        n->succ = succ;     // changing the adding link successor
-        n->succ->prev = n;} // changing the predecessor of the next element after the inserted to the inserted
-    this->succ = n;
-    //cout << this->description.name << " adds " << n->description.name << endl;
-    return n;
-}
-
-Link *Link::add_ordered(Link *n) {
-    if (n == nullptr) return this;
-    if (this == nullptr) {
-        cout << "nullptr -> Link::add\n";
-        return this;}
-    Link * p;
-    p = this;
-
-    while(p) {
-        if (!p->prev && !p->succ) break;
-        if (n->description.name < p->description.name) {
-            if (p->prev && n->description.name < p->prev->description.name) { p = p->prev;}
-            else return p->insert(n);
-        }
-        else if (n->description.name > p->description.name) {
-            if (p->succ  && n->description.name > p->succ->description.name) { p = p->succ; }
-            else return p->add(n);
-           }
-        else break;
+    while(p->next()){
+        p = p->next();
     }
-
-    if (n->description.name > p->description.name) {return p->add(n);}
-    return p->insert(n);
+    p->succ = n;
+    return this;
 }
 
