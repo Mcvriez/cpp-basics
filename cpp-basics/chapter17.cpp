06a6f7f22a76a64568f90d40a70d9b78d9448d56
mcvriez <mcvriez@gmail.com>
2020-03-16 14:56:46 +0200

    ex 2 chapter 18 findx function

--- cpp-basics/chapter17.cpp
+++ cpp-basics/chapter17.cpp
@@ -3,32 +3,36 @@ using namespace std;
 
 /*
 
-1. Write a function, char* strdup(const char*), that copies a C-style string into memory it allocates on the free store. Do not use any standard library functions. 
+2. Write a function, char* findx(const char* s, const char* x), that finds the first occurrence of the C-style string x in s. 
+Do not use any standard library functions. 
 Do not use subscripting; use the dereference operator * instead.
 
 */
 
-char* strdup(const char* cp) {
-    const char* ccp = cp; int counter = 1;
-    while (*ccp) {cout << *ccp << "-" << counter << endl; ++counter; ++ccp;}
-    char* r = new char[counter]; char* result = r;
-    while (*cp) {
-        *r = *cp;
-        ++r; ++cp; 
+char* findx(const char* s, const char* x) {
+    while (*s) {
+        char* p = const_cast<char*>(s);
+        const char* xp = x; 
+        while (*xp==*s) {
+            ++xp;++s;
+            if (!*xp) return p;
+            if(*xp!=*s) --s;
+        }
+        ++s;
     }
-    *r = 0;
-    return result;
+    return nullptr;
 }
 
 
 int main()
 {
-    const char* cp {"test"};
-    char* r = strdup(cp);
-    cout << cp << ":\t" << &cp << endl;
-    cout << r << ":\t" << &r << endl;
-    delete[] r;
+    const char* fp {"test b2 3d1kfsfs dfjs12121223lkjnk  111233  askd"};
+    const char* subs {"123"};
+    char* f = findx(fp, subs);
+    if (f) cout << f << endl;
+    else cout << "No result" << endl;
 
 }
+
 
 
