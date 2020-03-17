aac7e5c7149664cc2a13792929be8bb3c4e46c65
wq <mcvriez@gmail.com>
2020-03-17 15:56:08 +0200

    ex 8 chapter 18 palindromes

--- cpp-basics/chapter17.cpp
+++ cpp-basics/chapter17.cpp
@@ -3,43 +3,62 @@ using namespace std;
 
 /*
 
-5. Write a function, string cat_dot(const string& s1, const string& s2), that concatenates two strings with a dot in
-between. For example, cat_dot("Niels", "Bohr") will return a string containing Niels.Bohr.
+8. Rewrite all the functions in §18.7 to use the approach of making a backward copy of the string and then comparing; for example, take "home", generate "emoh", and compare those two strings to see that they are different, so home isn’t a palindrome.
 
-6. Modify cat_dot() from the previous exercise to take a string to be used as the separator (rather than dot) as its third argument.
-
-7. Write versions of the cat_dot()s from the previous exercises to take C-style strings as arguments
- and return a free-store-allocated C-style string as the result.
-Do not use standard library functions or types in the implementation. Test these functions with several strings.
-Be sure to free (using delete) all the memory you allocated from free store (using new).
-Compare the effort involved in this exercise with the effort involved for exercises 5 and 6.
 */
 
-string cat_dot(const string& s1, const string& s2, const string& sep) {
-    return s1 + sep + s2;
+bool is_palindrome(const string& s) {
+    string ss;
+    for (const char x : s ) {ss.insert(0, 1, x);}
+    return ss == s;
 }
 
-char* cat_dot_c(const char* c1, const char* c2, const char* sep) {
+bool is_palindrome_a(const char s[], int n) {
+    char backward[n];
+    const char* k = s;
+    while(n){
+        backward[n-1] = *k;
+        ++k; --n;
+    }
+    int i = 0;
+    while(backward[i]){
+        if (backward[i] != *s) {return false;}
+        ++i; ++s;
+    }
+    return true;
+}
+
+bool is_palindrome_s(const char* first, const char* last) {
+    const char* k = first;
     int count = 1;
-    const char* c = c1; while(*c){ ++count; ++c;}
-    c = c2; while(*c){ ++count; ++c;}
-    c = sep; while(*c){ ++count; ++c;}
-    char* res = new char[count];
-    res[count] = 0;
-    count = 0;
-    while(*c1){res[count] = *c1; ++c1;++count;}
-    while(*sep){res[count] = *sep; ++sep;++count;}
-    while(*c2){res[count] = *c2; ++c2;++count;}
-    return res;
+    int scount;
+    while(k != last){++count; ++k;}
+    scount = count;
+    k = first;
+    char arr[count];
+    while(count) {arr[count - 1] = *k; --count; ++k;}
+    while(scount){
+        if (*first != arr[count]) return false;
+        ++count;
+        ++first;
+        --scount;
+    }
+    return true;
 }
-
 
 int main()
 {
-    char s1[] = "Gogel"; char s2[] = "Barabek"; char sep[] = " Mogel ";
-    char* res = cat_dot_c(s1, s2, sep);
-    cout << res << endl;
-    delete[] res;
+    cout<< is_palindrome("kappa") << endl;
+    cout<< is_palindrome("kappak") << endl;
+
+    char sss[]{ "string" }; char ss[]{ "AaA" };
+    cout<< is_palindrome_a(sss, 6) << endl;
+    cout<< is_palindrome_a(ss, 3) << endl;
+
+    char* p1 = sss; char* p2 = &sss[5];
+    char* ps1 = ss; char* ps2 = &ss[2];
+    cout<< is_palindrome_s(p1, p2) << endl;
+    cout<< is_palindrome_s(ps1, ps2) << endl;
 }
 
 
